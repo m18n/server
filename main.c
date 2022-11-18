9b05517e22cb40cccee0f0043628cf2e382348b9
Misha <20xim04@gmail.com>
2022-11-18 15:50:14 +0200

    fix main

--- main.c
+++ main.c
@@ -2,16 +2,16 @@
 
 #include <pthread.h>
 #include"server.h"
-cl_server_t serv;
+sv_server_t serv;
 int main(){
     pthread_t process_pack;
     printf("HELLO ");
     
-    cl_ServerInit(&serv,9999,10);
+    sv_ServerInit(&serv,9999,10);
     //InitUserPack();
-    cl_v2_t v;
+    sv_v2_t v;
     v.p1=&serv.arrpack;
-    pthread_create( &process_pack, NULL, cl_arraypack_process, (void*) &v);
+    pthread_create( &process_pack, NULL, sv_arraypack_process, (void*) &v);
     //GetEventsSocket(&serv,sizeof(olxuser_t),CreateOlxUser,ClearOlxUser);
     return 0;
 }
